7707f6065acf9224e5b5ffb227533d75647dbe00
Sindre Sorhus <sindresorhus@gmail.com>
2019-06-30 12:06:38 +0700

    Upgrade to ESLint v6 and require Node.js 8
    Fixes #162

--- tasks/eslint.js
+++ tasks/eslint.js
@@ -4,7 +4,7 @@ const eslint = require('eslint');
 
 module.exports = grunt => {
 	grunt.registerMultiTask('eslint', 'Validate files with ESLint', function () {
-		const opts = this.options({
+		const options = this.options({
 			outputFile: false,
 			quiet: false,
 			maxWarnings: -1,
@@ -16,47 +16,47 @@ module.exports = grunt => {
 			return true;
 		}
 
-		const formatter = eslint.CLIEngine.getFormatter(opts.format);
+		const formatter = eslint.CLIEngine.getFormatter(options.format);
 
 		if (!formatter) {
-			grunt.warn(`Could not find formatter ${opts.format}`);
+			grunt.warn(`Could not find formatter ${options.format}`);
 			return false;
 		}
 
-		const engine = new eslint.CLIEngine(opts);
+		const engine = new eslint.CLIEngine(options);
 
 		let report;
 		try {
 			report = engine.executeOnFiles(this.filesSrc);
-		} catch (err) {
-			grunt.warn(err);
+		} catch (error) {
+			grunt.warn(error);
 			return false;
 		}
 
-		if (opts.fix) {
+		if (options.fix) {
 			eslint.CLIEngine.outputFixes(report);
 		}
 
 		let results = report.results;
 
-		if (opts.quiet) {
+		if (options.quiet) {
 			results = eslint.CLIEngine.getErrorResults(results);
 		}
 
 		const output = formatter(results);
 
-		if (opts.outputFile) {
-			grunt.file.write(opts.outputFile, output);
+		if (options.outputFile) {
+			grunt.file.write(options.outputFile, output);
 		} else if (output) {
 			console.log(output);
 		}
 
-		const tooManyWarnings = opts.maxWarnings >= 0 && report.warningCount > opts.maxWarnings;
+		const tooManyWarnings = options.maxWarnings >= 0 && report.warningCount > options.maxWarnings;
 
 		if (report.errorCount === 0 && tooManyWarnings) {
-			grunt.warn(`ESLint found too many warnings (maximum: ${opts.maxWarnings})`);
+			grunt.warn(`ESLint found too many warnings (maximum: ${options.maxWarnings})`);
 		}
 
-		return opts.failOnError ? report.errorCount === 0 : 0;
+		return options.failOnError ? report.errorCount === 0 : 0;
 	});
 };
